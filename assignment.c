8c0f899a096cde277812a227e08514ac57a96899
root <root@centro.gta.ufrj.br>
2018-11-12 18:53:21 -0200

    Funções estão corretas. Checar Método de Busca e de Minimização

--- assignment.c
+++ assignment.c
@@ -1,11 +1,20 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
+#include "stdlib.h"
+#include "stdio.h"
+#include "math.h"
 
 #define NUM 3
-#define max(a,b) ({ typeof (a) _a = (a); typeof (b) _b = (b); _a > _b ? _a : _b; })
+#define max(a,b) \
+	({__typeof__ (a) _a = (a);\
+	__typeof__ (b) _b = (b);\
+	_a > _b ? _a : _b;})
 
-typedef double (*func)(double*, double);
+#define compare(c,d) \
+	({__typeof__ (c) _c = (c);\
+	__typeof__ (d) _d = (d);\
+	_c > _d ? 1 : ( _c < _d ? -1 : 0 );})
+
+typedef double (*rst)(double*);
+typedef double (*func)(double*, double, rst);
 
 double* vectorSum(double* x1, double* x2, int size){
 	double* res = calloc(size, sizeof(double));
@@ -38,34 +47,51 @@ int scalarEqual(double* x1, double scalar, int size){
 }
 
 
+double restr(double* vars){
+	return pow(vars[0], 2.0) + pow(vars[1], 2.0) + pow(vars[2], 2.0) - 1;
+}
 
-double funcao(double* vars, double rho){
+
+double funcao(double* vars, double rho, rst restr){
 	double resultado = pow(vars[0], 3.0) + pow(vars[1], 3.0) + pow(vars[2], 3.0);
-	double restricao = pow(vars[0], 2.0) + pow(vars[1], 2.0) + pow(vars[2], 2.0) - 1;
-	restricao = max(0, pow(restricao, 2));
+	double restricao = restr(vars);
+
+	restricao = pow(restricao, 2);
 	return resultado + rho*restricao;
 }
 
 
 
-double* gradiente(double* vars){
+double* gradiente(double* vars, double rho, rst restr){
 	double* grad;
+	double t = 4*rho*restr(vars);
 	grad = (double*) calloc(3, sizeof(double));
-	for (int i = 0; i < 3; i++) grad[i] = 3*pow(vars[i], 2);
+	for (int i = 0; i < 3; i++){
+		grad[i] = 3*pow(vars[i], 2) + vars[i]*t;
+	}
 	return grad;
 }
 
-double** segundoGradiente(double* vars){
+double** segundoGradiente(double* vars, double rho, rst restr){
 	double** segGrad;
+	double t = 4*rho*restr(vars);
 	segGrad = (double**) calloc(3, sizeof(double*));
 	for (int i = 0; i < 3; i++){
 		segGrad[i] = (double*) calloc(3, sizeof(double));
-		segGrad[i][i] = 6*vars[i];
+		
+		double temp = 8*vars[i]*rho;
+		segGrad[i][i] = 6*vars[i] + temp*vars[i] + t;
+		
+		for (int j = 0; j < i; j++){
+			double temp2 = temp*vars[j];
+			segGrad[i][j] = temp*vars[j];
+			segGrad[j][i] = temp*vars[j];
+		}
 	}
 	return segGrad;
 }
 
-double* buscaDeArmijo(func f, double* vars, double t, double* gradiente, double y, double rho){
+/*double* buscaDeArmijo(func f, double* vars, double t, double* gradiente, double y, double rho){
 	double resultadoReduzido, resultado, comparador, reducaoProporcional;
 
 	double* temp = scalarMult(gradiente, -t, 3);
@@ -115,10 +141,33 @@ void metodoDoGradiente(func f, double* vars, int reps, int parada, double rho){
 		if (parada == 0 && delta < 1.0e-04) break;
 		rho *= beta;
 	}
-}
+}*/
 
-int main(){
-	double x[3] = {0.0,2.0,40.0};
-	metodoDoGradiente(funcao, x, 10000, 0, 4.0);
-	for (int i = 0; i < 3; i++) printf("%f\n", x[i]);
+void main(){
+	
+	double x[3] = {1,2,3};
+	double rho = 9;
+	
+	double res = funcao(x, rho, restr);
+	printf("%f\n", res);
+	
+	printf("\n");
+
+	double* grad = gradiente(x, rho, restr);
+	for (int i = 0; i < 3; i++)
+	{
+		printf("%f\n", grad[i]);
+	}
+
+	printf("\n");
+	
+	double** secGrad = segundoGradiente(x, rho, restr);
+	for (int i = 0; i < 3; i++)
+	{	
+		for (int j = 0; j < 2; j++)
+		{
+			printf("%f,\t", secGrad[i][j]);
+		}
+		printf("%f\n", secGrad[i][2]);
+	}
 }
