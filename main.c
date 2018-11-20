695d703fdfad85f3bf313df0fe5ba9bfcf96d990
LucSan10 <santiagolucas643@poli.ufrj.br>
2018-11-20 18:07:57 +0000

    FINAL MESMO

--- main.c
+++ main.c
@@ -9,8 +9,8 @@
 void main(){
 	
 	char* info;
-	double x[3] = {-3,-2,-1};
-	double parametro_de_restricao = 10.0;
+	double x[3] = {-3,-50,-4};
+	double parametro_de_restricao = 4.0;
 	double x0[3];
 
 	for (int i = 0; i < NUM; i++)
@@ -23,26 +23,50 @@ void main(){
 	printf("x0:\n");
 	for (int i = 0; i < NUM; i++)
 	{
-		printf("%f\n", x0[i]);
+		printf("(%f)\n", x0[i]);
 	}
 	
 	printf("\n");
 	printf("x* tq f(x*) é mínimo:\n");
 	for (int i = 0; i < NUM; i++)
 	{
-		printf("%f\n", x[i]);
+		printf("(%f)\n", x[i]);
 	}
 
+	int k = 0;
 	double delta[3] = {0.0,0.0,0.0};
+	double d[3] = {0.0,0.0,0.0};
+	double comp[3] = {-1.0,0.0,0.0};
+
 	printf("\nValor Esperado:\n");
 	for (int i = 0; i < NUM; i++)
 	{
-		printf("%f\n", -1.0/sqrt(3));
-		delta[i] = x[i] + 1.0/sqrt(3);
+		for (int j = 0; j < NUM; ++j)
+		{
+			d[j] = x[j];
+		}
+
+		d[i] += 1;
+		delta[i] = norm2(d,NUM);
+		if (delta[i] < delta[k]){
+			comp[k] = 0.0;
+			k = i;
+			comp[k] = -1.0;
+		}
 	}
 
-	printf("\n||Erro Absoluto||2 = %f\n", norm2(delta ,NUM));
+	for (int i = 0; i < NUM; ++i)
+	{
+		printf("%f\n", comp[i]);
+	}
 
-	printf("\nf(x*) = %f\n", funcao_penalizada(x, 0.0));
-	printf("%s\n", info);
+	double fx1 = funcao_penalizada(x, 0.0);
+	double fx0 = funcao_penalizada(comp, 0.0);
+
+	printf("\n||Erro Absoluto(x)||2 = %f\n", delta[k]);
+	
+	printf("Erro Absoluto(f(x)) = %f\n", fabs(fx1-fx0));
+
+	printf("\nf(x*) = %f\n", fx1);
+	printf("Condição de parada: %s\n", info);
 }
